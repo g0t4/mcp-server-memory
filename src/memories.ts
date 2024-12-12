a52ef9c720a3311d1d43b7df44b73e0133e3469d
Wes Higbee <wes.mcclure@gmail.com>
2024-12-12 00:27:00 -0600

    rework

--- src/memories.ts
+++ src/memories.ts
@@ -4,47 +4,27 @@ import { always_log, verbose_log } from "./logs.js";
 // TODO add configurable path to this file (will fix pathing issues too)
 // FYI pathing is to workaround no support for a cwd in claude_desktop_config.json (yet?)
 import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
+import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-let reminders_file_path = __dirname + "/reminders.txt";
+let memories_file_path = __dirname + "/memories.txt";
+verbose_log("INFO: memories file path", memories_file_path);
 
-const requestedReminders = process.argv.includes("--reminders");
-// check for --reminders PATH in CLI args
-if (requestedReminders) {
-    // TODO add CLI arg parser instead of this hack
-    const flag_index = process.argv.indexOf("--reminders");
-    if (flag_index + 1 >= process.argv.length) {
-        always_log("ERROR: reminders file path not specified");
-        process.exit(1);
-    }
-    reminders_file_path = process.argv[flag_index + 1];
-    // need absolute path:
-    reminders_file_path = resolve(process.cwd(), reminders_file_path);
-}
-verbose_log("INFO: reminders file path", reminders_file_path);
-
-export async function readReminders(): Promise<String> {
-    // TODO only pass back reminders file if requested?
-    let file_reminder =
-        "Here is the reminders file if you want to add to it: " +
-        reminders_file_path;
-    // if the file doesn't exist, treat that as NO reminders
+export async function readMemories(): Promise<String> {
+    // if the file doesn't exist, treat that as NO memories
     const file_exists = await fs
-        .access(reminders_file_path, fs.constants.F_OK)
+        .access(memories_file_path, fs.constants.F_OK)
         .then(() => true) // if does exist, set to true
         .catch(() => false); // error callback only invoked if does not exist
     if (!file_exists) {
         // dont wanna log failures when the file is just not there
-        return file_reminder;
+        return "";
     }
 
     try {
-        const reminders =
-            (await fs.readFile(reminders_file_path, "utf8")) ?? "";
-        return reminders + "\n" + file_reminder;
+        return (await fs.readFile(memories_file_path, "utf8")) ?? "";
     } catch (error) {
-        always_log("WARN: reading reminders failed", error);
-        return file_reminder;
+        always_log("WARN: reading memories file failed", error);
+        return "";
     }
 }
